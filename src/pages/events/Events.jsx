6d4523a713706651f8281a6ab8bec8f32aa05e32
03Ahamedsalman <ahamedsalman800@gmail.com>
2025-02-17 10:56:35 +0530

    third commit

--- src/pages/events/Events.jsx
+++ src/pages/events/Events.jsx
@@ -16,13 +16,8 @@ const Events = () => {
   return (
     <>
       <div style={{ backgroundImage: `url(${eventbg})` }}>
-<<<<<<< HEAD
-        <div className="w-full text-center pt-20 h-10  flex justify-center items-center">
-          <h1 className="text-4xl font-bold">Upcoming Events</h1>
-=======
         <div className="w-full text-center mb-8 h-40 flex justify-center items-center">
           <h2 className="text-4xl font-bold">Upcoming Events</h2>
->>>>>>> 82ed14e0ad93dd905791dd840119d50fef79d20e
         </div>
         <div className="p-10 flex justify-center">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
